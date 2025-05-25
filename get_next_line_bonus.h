35926ab401e1eb4fdd7cd6647e3de942ba13129e
Jaehun Lee <jaehlee@c2r8s8.42gyeongsan.kr>
2025-05-25 15:20:15 +0900

    get next line

---
+++ get_next_line_bonus.h
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jaehlee <jaehlee@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/14 13:45:30 by jaehlee           #+#    #+#             */
+/*   Updated: 2025/05/14 13:45:30 by jaehlee          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1024
+# endif
+
+# ifndef OPEN_MAX
+#  define OPEN_MAX 32
+# endif
+
+int		ft_strlen(const char *str);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*get_next_line(int fd);
+int		ft_strlcpy(char *dest, const char *src, size_t size);
+char	*ft_strdup(char *s);
+int		is_nl(char *str);
+
+#endif
